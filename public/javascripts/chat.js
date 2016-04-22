54ac83233c7ef05c4414ca4f2dfef274af9bdd9f
leechart <leep12340@gmail.com>
2016-04-22 17:13:30 +0800

    modify

---
+++ public/javascripts/chat.js
@@ -0,0 +1,33 @@
+var Chat = function (socket) {
+    this.socket = socket;
+}
+
+Chat.prototype.sendMessage = function(room, text) {
+    var message = {room:room,text:text};
+    this.socket.emit('message',message);
+}
+Chat.prototype.changeRoom = function(room) {
+    this.socket.emit('join', {newRoom:room});
+}
+Chat.prototype.proccessCommand = function(command) {
+    var words   = command.split(' ');
+    var command = words[0].substring(1, words[0].length).toLowerCase();
+    var massage = false;
+    switch (command) {
+        case 'join':
+            words.shift();
+            var room = words.join(' ');
+            this.changeRoom(room);
+            break;
+        case 'nick':
+            words.shift();
+            var name = words.join(' ');
+            this.socket.emit('nameAttempt', name);
+            break;
+        default:
+            message = "unrecogized command.";
+            break;
+    }
+    return message;
+}
+
